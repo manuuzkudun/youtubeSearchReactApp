147d4e6579791bca18b84f5be54b420f32c9026c
Manu Uzkudun <manuuzkudun@gmail.com>
2016-09-11 17:09:42 +0200

    youtube search api implemented

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 //import video_detail from '/src/components/video_detail';
 //import video_list from '/src/components/video_list';
 
 const API_KEY = "AIzaSyDO6csmDqhY8WzXEmHvLU98oC29j2JheYQ";
+
+YTSearch({key: API_KEY, term: 'one piece'}, data => console.log(data));
 
 // Create a component (HTML) using JSX
 const App = () => {
